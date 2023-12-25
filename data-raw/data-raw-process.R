9ed96a63223797bc0e079a9eba869ea029f7fc10
Diego Mabe <dmabe@umag.cl>
2023-12-24 22:28:45 -0300

    estandarización variables
    - nombres estandarizados en col-names.txt
    - rename en proceso

--- data-raw/data-raw-process.R
+++ data-raw/data-raw-process.R
@@ -8,9 +8,25 @@ library(data.table)
 raw <- fread(input = "data-raw/data-raw.csv")
 
 # Data wrangling ----------------------------------------------------------
+colnames(raw)
+View(raw)
+ ##eliminar columnas repetidas sin valores
+raw_rename <- raw [, -"Complete?"]
+raw_rename <- raw_rename [, -"Survey Timestamp"]
+
+##renombrar columnas repetidas con valores
+names(raw_rename)[86] <- "bai_total"
+colnames(raw_rename)
+
+##renombrar columnas
+sapply(raw_rename, class)
+View(raw)
+
+
 
 ## Observar los valores unicos de cada variable
 lapply(raw, unique)
+
 
 
 
