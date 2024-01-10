a08923d500e39908dee9f88a98d2516c3321029c
Matías Castillo <matcasti@umag.cl>
2024-01-10 19:58:18 -0300

    finished formating on variables

--- data-raw/data-raw-process.R
+++ data-raw/data-raw-process.R
@@ -6,7 +6,7 @@ library(data.table)
 # Import dataset ----------------------------------------------------------
 
 raw <- fread(input = "data-raw/data-raw.csv",
-             col.names = readLines("data-raw/col-names.txt"))
+             col.names = readLines("data-raw/helpers/col-names.txt"))
 
 # Data wrangling ----------------------------------------------------------
 
@@ -16,7 +16,7 @@ raw <- raw[, .SD, .SDcols = grep("rm_", names(raw), value = TRUE, invert = TRUE)
 
 ## Add intervention groups using external lookup table and transform into a
 ## factor variable
-groups <- fread("data-raw/intervention_groups.csv")
+groups <- fread("data-raw/helpers/intervention_groups.csv")
 raw <- raw[groups, on = "id_record"]
 raw[, trt_group := factor(trt_group, levels = c("Control", "Ejercicio", "Combinado"))]
 rm(groups)
@@ -73,6 +73,11 @@ raw[, (ind) := lapply(.SD, function(i) {
 }), .SDcols = ind]
 rm(ind)
 
+#> sppb_categoria <- standardize as factor
+raw[, sppb_categoria := factor(sppb_categoria, 
+                               levels = c("Limitación mínima o sin limitación (AUTÓNOMO)", "Limitación leve (Frágil-Prefrágil)", "Limitación moderada (Frágil)"),
+                               labels = c("Autónomo", "Pre-Frágil", "Frágil"))]
+
 #> spaq_patron_verano/invierno <- standardize as factors of two levels
 raw[, spaq_patron_verano := factor(spaq_patron_verano, levels = c("Sí", "No"))]
 raw[, spaq_patron_invierno := factor(spaq_patron_invierno, levels = c("Sí", "No"))]
@@ -107,24 +112,72 @@ raw[, katz_categoria := factor(katz_categoria,
                                           "C (Independiente en todas las funciones menos en el baño y en otra una cualquiera)"),
                                labels = c("Independiente", "Excepto en una función", "Excepto en dos funciones"))]
 
+#> tm_categoria <- standardize as factor
+raw[, tm_categoria := factor(tm_categoria, 
+                             levels = c("BAJO RANGO", "NORMAL", "ALTO RANGO"),
+                             labels = c("Bajo", "Normal", "Alto"))]
+
 #> cc_imc_categoria <- standardize as ordered factor
 raw[, cc_imc_categoria := factor(cc_imc_categoria, 
                                  levels = c("Peso normal", "Sobrepeso", "Obeso"),
                                  ordered = TRUE)]
 
-#> cc_grasa_total_porcentaje <- change into number, remove text and commas
-raw[grepl("No", cc_grasa_total_porcentaje), cc_grasa_total_porcentaje := NA]
-raw[nchar(cc_grasa_total_porcentaje) == 0, cc_grasa_total_porcentaje := NA]
-raw[grepl("\\,", cc_grasa_total_porcentaje), 
-    cc_grasa_total_porcentaje := gsub("\\,", "\\.", cc_grasa_total_porcentaje)]
-raw[, cc_grasa_total_porcentaje := as.numeric(cc_grasa_total_porcentaje)]
+## Function to change missformated numbers into numeric format
+text_to_number <- function(x, remove_dash = TRUE) {
+  x[grepl("[A-Za-z]", x) | nchar(x) == 0] <- NA
+  x <- gsub("\\,", "\\.", x)
+  if (remove_dash) {
+    x <- gsub("\\-|\\*", "", x)
+  }
+  x <- as.numeric(x)
+}
 
+#> cc_grasa_total_porcentaje <- change into number, remove text and commas
+raw[, cc_grasa_total_porcentaje := text_to_number(cc_grasa_total_porcentaje)]
 
 #> cc_grasa_pierna_izquierda_porcentaje <- change into number and remove commas
-#> cc_grasa_torso_porcentaje <- change to number
-#> cc_masa_muscular_brazo_derecho <- change into number and remove commas
-#> cc_peso_corporal <- 
+raw[, cc_grasa_pierna_izquierda_porcentaje := text_to_number(cc_grasa_pierna_izquierda_porcentaje)]
 
+#> cc_grasa_torso_porcentaje <- change to number
+raw[, cc_grasa_torso_porcentaje := text_to_number(cc_grasa_torso_porcentaje)]
+
+#> cc_masa_muscular_brazo_derecho <- change into number and remove commas
+raw[, cc_masa_muscular_brazo_derecho := text_to_number(cc_masa_muscular_brazo_derecho)]
+
+#> cc_peso_corporal <- change to number
+raw[, cc_peso_corporal := text_to_number(cc_peso_corporal)]
+
+#> cc_masa_osea_porcentaje <- change to number
+raw[, cc_masa_osea_porcentaje := text_to_number(cc_masa_osea_porcentaje)]
+
+#> cc_edad_calculada <- change to number
+raw[, cc_edad_calculada := text_to_number(cc_edad_calculada)]
+
+#> hrv_pns_pre <- change to number
+raw[, hrv_pns_pre := text_to_number(hrv_pns_pre, remove_dash = FALSE)]
+
+#> hrv_sns_pre <- change to number
+raw[, hrv_sns_pre := text_to_number(hrv_sns_pre, remove_dash = FALSE)]
+
+#> hrv_rmssd_peri <- change to number
+raw[, hrv_rmssd_peri := gsub("\\-", "\\.", hrv_rmssd_peri)]
+raw[, hrv_rmssd_peri := text_to_number(hrv_rmssd_peri, remove_dash = FALSE)]
+
+#> hrv_pns_peri <- change to number
+raw[, hrv_pns_peri := text_to_number(hrv_pns_peri, remove_dash = FALSE)]
+
+#> hrv_pns_post <- change to number
+raw[, hrv_pns_post := text_to_number(hrv_pns_post, remove_dash = FALSE)]
+
+#> hrv_sns_post <- change to number
+raw[, hrv_sns_post := text_to_number(hrv_sns_post, remove_dash = FALSE)]
+
+#> bdnf_concentracion <- change to number
+raw[, bdnf_concentracion := text_to_number(bdnf_concentracion, remove_dash = FALSE)]
+
+mindfulness <- raw
 
 # Data export -------------------------------------------------------------
 
+save(mindfulness, file = "data/mindfulness.RData")
+fwrite(mindfulness, file = "data-raw/minduflness.csv")
